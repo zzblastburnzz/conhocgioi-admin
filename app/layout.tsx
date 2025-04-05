20220d67872d6145577180164e387c582a594f6b
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-05 08:46:42 +0700

    update

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+// app/layout.tsx
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  );
+}
