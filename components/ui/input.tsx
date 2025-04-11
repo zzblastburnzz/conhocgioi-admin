b30cc4f75087bd6f34e27f9623327be635f81d2e
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-11 16:01:47 +0700

    🧹 Cleanup: removed large file from history

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,10 @@
-// dummy
+import * as React from "react";
+
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className="border rounded-md px-3 py-2 w-full"
+      {...props}
+    />
+  );
+}
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,1 +1,10 @@
-// dummy
+import * as React from "react";
+
+export function Textarea(props: React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return (
+    <textarea
+      className="border rounded-md px-3 py-2 w-full"
+      {...props}
+    />
+  );
+}
